0ef7a3d0487ad397775c0237946ddb5a5f57133a
RunsWritesCode <sophieamullins@gmail.com>
2018-01-09 16:21:35 +0000

    can click on Summit in Summit List and go to Summit Item. Toggle button added for climbed or not climbed but not code to update summit yet

--- app/src/main/java/com/example/codeclan/summit/SummitActivity.java
+++ app/src/main/java/com/example/codeclan/summit/SummitActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 /**
  * Created by sophiemullins on 06/01/2018.
@@ -16,16 +17,30 @@ import android.view.MenuItem;
 
 public class SummitActivity extends AppCompatActivity {
 
+    TextView name;
+    TextView height;
+    TextView range;
+    TextView country;
+
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_summits);
+            setContentView(R.layout.summit_details);
+
+            name = findViewById(R.id.name);
+            height = findViewById(R.id.height);
+            range = findViewById(R.id.range);
+            country = findViewById(R.id.country);
 
             Intent intent = getIntent();
             Summit summit = (Summit)intent.getSerializableExtra("summit");
             Log.d("Summit name:", summit.getName());
 
-            
+            name.setText(summit.getName());
+            height.setText(summit.getHeight().toString());
+            range.setText(summit.getRange());
+            country.setText(summit.getCountry());
+
         }
 
     @Override
