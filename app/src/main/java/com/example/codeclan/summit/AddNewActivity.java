7a956942a027765e3c0ea3bcb27a64ee51685490
RunsWritesCode <sophieamullins@gmail.com>
2018-01-10 10:42:53 +0000

    added shared preferences

--- app/src/main/java/com/example/codeclan/summit/AddNewActivity.java
+++ app/src/main/java/com/example/codeclan/summit/AddNewActivity.java
@@ -1,9 +1,13 @@
 package com.example.codeclan.summit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.EditText;
 
 public class AddNewActivity extends AppCompatActivity {
 
@@ -19,4 +23,17 @@ public class AddNewActivity extends AppCompatActivity {
         menuInflater.inflate(R.menu.activity_main, menu);
         return true;
     }
+
+    public void onSubmitButtonClicked(View button) {
+        Log.d(getClass().toString(), "onSubmitButtonClicked was called");
+        EditText nameField = (EditText) findViewById(R.id.editName);
+        Log.d("NAME", nameField.getText().toString());
+
+    }
+
+
+
+
+//    onClickSubmit { get back from shared prefs, convert to java arraylist, arraylist.add(new summit), convert back to string and saved back to SPrefs.
+
 }
--- app/src/main/java/com/example/codeclan/summit/SummitsActivity.java
+++ app/src/main/java/com/example/codeclan/summit/SummitsActivity.java
@@ -1,6 +1,8 @@
 package com.example.codeclan.summit;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +11,9 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
 
@@ -19,8 +24,27 @@ public class SummitsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summits);
 
-        Summits summits = new Summits();
-        ArrayList<Summit> list = summits.getList();
+        SharedPreferences sharedPref = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+
+        String mySummits = sharedPref.getString("MySummits", new ArrayList<Summit>().toString());
+
+        Gson gson = new Gson();
+        TypeToken<ArrayList<Summit>> summitArrayList = new TypeToken<ArrayList<Summit>>(){};
+        ArrayList<Summit> list = gson.fromJson(mySummits, summitArrayList.getType());
+        Log.d("mySummits", list.toString());
+
+//        replace next 2 lines with data from shared prefs. convert to java arraylist.
+
+
+//        Summits summits = new Summits();
+//        ArrayList<Summit> list = summits.getList();
+
+//        SharedPreferences sharedPref = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+//        SharedPreferences.Editor editor = sharedPref.edit();
+//
+//        Gson gson = new Gson();
+//        editor.putString("MySummits", gson.toJson(list));
+//        editor.apply();
 
         SummitsAdapter summitAdapter = new SummitsAdapter(this, list);
         ListView listView = (ListView) findViewById(R.id.list);
